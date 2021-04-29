ce5d4a1bad1437199f9c8ad723b82bdd4148f76b
Jonas Estevam <jonas_estevam@live.com>
2021-04-29 15:34:03 -0300

    feat: connecting to the database

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 require("dotenv").config();
 import express from "express";
+import mongoose from "mongoose";
 import { routes } from "./routes";
 
 const app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    err ? console.log(err) : console.log("Banco de dados conectado!");
+  }
+);
 
 app.use(express.json());
 app.use("/api", routes);
--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -3,7 +3,6 @@ import { Request, Response } from "express";
 class User {
   create(req: Request, res: Response) {
     const { username, password } = req.body;
-
     return res.json({ username, password });
   }
 }
