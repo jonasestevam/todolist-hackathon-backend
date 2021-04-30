b82ac1ad6b81fa1ecf54fb7d79341f3f3f1106b2
Jonas Estevam <jonas_estevam@live.com>
2021-04-30 11:50:18 -0300

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 require("dotenv").config();
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { routes } from "./routes";
 
 const app = express();
+app.use(cors());
 
 mongoose.connect(
   process.env.MONGODB_URI,
